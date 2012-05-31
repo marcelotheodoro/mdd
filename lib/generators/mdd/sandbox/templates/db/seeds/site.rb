49cb0b1356f274af6e1aa65c1e94994c7cfdf177
Marcelo <marcelo.theodorojr@gmail.com>
2012-05-31 11:52:50 -0300

    Language added ajax scaffold Devise initializer updated

--- lib/generators/mdd/sandbox/templates/db/seeds/site.rb
+++ lib/generators/mdd/sandbox/templates/db/seeds/site.rb
@@ -1,5 +1,5 @@
 # -*- encoding : utf-8 -*-
-if Administrator.count == 0
+if Administrator.count.zero?
 	Permission.create( :name => "SuperAdmin" ) if Permission.find_by_name("SuperAdmin").nil?
 	Administrator.create :name => "Administrator", :email => 'admin@admin.com', :password => 'admin123', :password_confirmation => 'admin123'
 end
