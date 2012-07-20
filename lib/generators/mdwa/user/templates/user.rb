8c42f77d43ea838306eed2780dd41ae4c1ce1ef9
Marcelo <marcelo.theodorojr@gmail.com>
2012-07-20 16:17:59 -0300

    Users generator

---
+++ lib/generators/mdwa/user/templates/user.rb
@@ -0,0 +1,11 @@
+# -*- encoding : utf-8 -*-
+MDWA::DSL.users.register '<%= @user %>' do |u|
+  
+  # Description of what this user does in the system.
+  # u.description = 'Member of the programmers team.'
+  
+  # User roles
+  # By default, the user name is already included by default.
+  # u.user_roles = ['<%= @user %>']
+  
+end
--- lib/generators/mdwa/user/user_generator.rb
+++ lib/generators/mdwa/user/user_generator.rb
@@ -1,21 +1,29 @@
 require 'rails/generators/migration'
-require 'mdwa/generators'
+require 'mdwa/dsl'
 
 module Mdwa
-  
   module Generators
 
     class UserGenerator < Rails::Generators::Base
 
       source_root File.expand_path('../templates', __FILE__)
       
+      attr_accessor :user
+      
       argument :user_name
 
       def initialize(*args, &block)
         super
+        
+        @user = user_name.singularize.camelize
+      end
+    
+      def generate_user
+        template 'user.rb', "#{MDWA::DSL::USERS_PATH}#{@user.underscore}.rb"
+        generate "mdwa:entity #{@user} --user"
       end
 
-    end
+    end # class user generator
 
-  end 
-end
+  end # generators
+end # mdwa
