cb6ad326e6431281f2d5590245f3c8fc8744eca1
Marcelo Theodoro <marcelo.theodorojr@gmail.com>
2012-08-27 14:29:17 -0300

    Software visualization only for development.

---
+++ app/controllers/mdwa/mdwa_controller.rb
@@ -0,0 +1,8 @@
+class Mdwa::MDWAController < ApplicationController
+  
+  protected
+      def restrict_to_development
+        head(:bad_request) unless Rails.env.development?
+      end
+  
+end
--- app/controllers/mdwa/requirements_controller.rb
+++ app/controllers/mdwa/requirements_controller.rb
@@ -1,10 +1,9 @@
 require 'mdwa/dsl'
-class Mdwa::RequirementsController < ApplicationController
+class Mdwa::RequirementsController < Mdwa::MDWAController
   
   def index
     require_all MDWA::DSL::REQUIREMENTS_PATH
     @requirements = MDWA::DSL.requirements.all
-    
   end
   
 end
