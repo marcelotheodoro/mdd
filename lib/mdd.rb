89e705340373eeb81b465e240e87c1926147ab95
Marcelo Theodoro <marcelo.theodorojr@gmail.com>
2012-10-02 14:31:42 -0300

    Fix load path for to_alias method.

--- lib/mdd.rb
+++ lib/mdd.rb
@@ -1,5 +1,6 @@
 # -*- encoding : utf-8 -*-
 require 'rails/engine'
+require 'extensions/string'
 
 module MDD
   class MDD < Rails::Engine
--- lib/mdwa.rb
+++ lib/mdwa.rb
@@ -1,8 +1,6 @@
 # -*- encoding : utf-8 -*-
 require "mdwa/version"
 
-require 'extensions/string'
-
 module MDWA
 
 end
